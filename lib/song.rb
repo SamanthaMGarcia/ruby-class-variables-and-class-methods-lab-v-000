b3ae6c45b7ccfee44cd04738edb7393d8619ffe5
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-15 22:25:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,15 @@ class Song
   @@artists unique
   end
   
-  # def artist_count
-  #   hash of key value pairs of artists and their count using @@arists
-  # end
-     
+   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
