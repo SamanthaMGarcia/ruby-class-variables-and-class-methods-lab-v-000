f63eb23ae51efae561d0ac7781c140861b6a499f
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-15 22:19:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,14 +21,14 @@ class Song
   @@genres unique
   end
   
+  # def self.genre_count
+  #   hash of key value pairs of genres and their count using @@genres
+  # end
+  
   def self.artists
   @@artists unique
   end
   
-  # def self.genre_count
-  #   hash of key value pairs of genres and their count using @@genres
-  # end
-
   # def artist_count
   #   hash of key value pairs of artists and their count using @@arists
   # end
